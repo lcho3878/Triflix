22bdca25ff81c51e4cd7e3dc262f50312a498f3a
saengle <100755761+saengle@users.noreply.github.com>
2024-10-10 05:11:34 +0900

    [Feat] TrendingView 레이아웃 대략 잡아두기

--- Triflix/Sources/Presentation/TrendingView.swift
+++ Triflix/Sources/Presentation/TrendingView.swift
@@ -1,4 +1,171 @@
+//
+//  TrendingView.swift
+//  Triflix
+//
+//  Created by 쌩 on 10/9/24.
+//
+
 import UIKit
 import SnapKit
+
 final class TrendingView: BaseView {
+    
+    let sparkleItem = UIBarButtonItem()
+    let magnifyingglassItem = UIBarButtonItem()
+    let scrollView = {
+        let sv = UIScrollView()
+        sv.isScrollEnabled = true
+        return sv
+    }()
+    let myView = UIView()
+    let mainPosterImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "star")
+        iv.backgroundColor = .white
+        return iv
+    }()
+    let mainPosterGenreLabel = {
+        let lb = UILabel()
+        lb.text = "어쩌구 저쩌구 라벨이름 블라블라"
+        return lb
+    }()
+    let playButton  = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.subtitle = "재생" // text font size : Text Attributes통해 조절
+        configuration.image = UIImage(systemName: "play.fill")
+        configuration.imagePadding = 10
+        configuration.baseBackgroundColor = .white
+        configuration.baseForegroundColor = .black
+        let bt = UIButton(configuration: configuration)
+        return bt
+    }()
+    let favoriteButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.subtitle = "내가 찜한 리스트" // text font size : Text Attributes통해 조절
+        configuration.image = UIImage(systemName: "plus")
+        configuration.imagePadding = 10
+        configuration.baseBackgroundColor = .black
+        configuration.baseForegroundColor = .white
+        let bt = UIButton(configuration: configuration)
+        return bt
+    }()
+    let trendingMovieLabel = {
+        let lb = UILabel()
+        lb.text = "지금 뜨는 영화"
+        return lb
+    }()
+    let trendingMovieCV = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    let trendingTVLabel = {
+        let lb = UILabel()
+        lb.text = "지금 뜨는 TV 시리즈"
+        return lb
+    }()
+    let trendingTVCV =  UICollectionView(frame: .zero, collectionViewLayout: layout())
+    
+    static func layout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 40, height: 120) // 추후 사이즈 조절
+        layout.scrollDirection = .horizontal
+        return layout
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func configureUI() {
+        self.backgroundColor = .black
+        sparkleItem.image = UIImage(systemName: "sparkles.tv")
+        sparkleItem.tintColor = .white
+        magnifyingglassItem.image = UIImage(systemName: "magnifyingglass")
+        magnifyingglassItem.tintColor = .white
+        //아래 보기 편하라고 만든 색상들 추후 수정(제거)
+        scrollView.backgroundColor = .brown
+        myView.backgroundColor = .clear
+        trendingMovieCV.backgroundColor = .blue
+        trendingTVCV.backgroundColor = .red
+    }
+    
+    override func addSubviews() {
+        addSubview(scrollView)
+        scrollView.addSubview(myView)
+        [mainPosterImageView, mainPosterGenreLabel, favoriteButton, playButton, trendingMovieLabel,
+         trendingMovieCV,
+         trendingTVLabel,
+         trendingTVCV
+        ].forEach {myView.addSubview($0)}
+    }
+    
+    override func setConstraints() {
+        
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        myView.snp.makeConstraints{
+            $0.edges.equalTo(scrollView)
+            $0.width.equalTo(scrollView.snp.width)
+        }
+        
+        mainPosterImageView.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview()
+            $0.width.equalTo(safeAreaLayoutGuide.snp.width).inset(16)
+//            $0.height.equalTo(mainPosterImageView.snp.width)
+            $0.height.equalTo(400) // size 조절 필요
+        }
+        
+        mainPosterGenreLabel.snp.makeConstraints {
+            $0.bottom.equalTo(playButton.snp.top).offset(-16)
+            $0.centerX.equalTo(mainPosterImageView.snp.centerX )
+        }
+        
+        playButton.snp.makeConstraints{
+            $0.height.equalTo(32)
+            $0.width.equalTo((150))
+            $0.leading.equalTo(mainPosterImageView.snp.leading).inset(16)
+            $0.trailing.equalTo(mainPosterImageView.snp.centerX).inset(4)
+            $0.bottom.equalTo(mainPosterImageView.snp.bottom).inset(16)
+        }
+        
+        favoriteButton.snp.makeConstraints{
+            $0.height.equalTo(32)
+            $0.width.equalTo(150)
+            $0.leading.equalTo(mainPosterImageView.snp.centerX).offset(4)
+            $0.trailing.equalTo(mainPosterImageView.snp.trailing).inset(16)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(mainPosterImageView.snp.bottom).inset(16)
+        }
+        
+        trendingMovieLabel.snp.makeConstraints{
+            $0.top.equalTo(mainPosterImageView.snp.bottom).offset(16)
+            $0.horizontalEdges.equalTo(mainPosterImageView.snp.horizontalEdges).inset(16)
+            $0.height.equalTo(20)
+        }
+        
+        trendingMovieCV.snp.makeConstraints{
+            $0.top.equalTo(trendingMovieLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(120)
+        }
+        
+        trendingTVLabel.snp.makeConstraints{
+            $0.top.equalTo(trendingMovieCV.snp.bottom).offset(16)
+            $0.horizontalEdges.equalTo(mainPosterImageView.snp.horizontalEdges).inset(16)
+            $0.height.equalTo(20)
+        }
+        
+        trendingTVCV.snp.makeConstraints{
+            $0.top.equalTo(trendingTVLabel.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(120)
+            $0.bottom.equalTo(myView).inset(16) // 아래 여백 필요시 조절 및 제거
+        }
+    }
+}
+// 필요 없으면 제거 필요시 사용 후 주석 수정
+extension TrendingView {
 }
--- Triflix/Sources/Presentation/TrendingViewController.swift
+++ Triflix/Sources/Presentation/TrendingViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import RxCocoa
 import RxSwift
 
-class TrendingViewController: UIViewController {
-
+final class TrendingViewController: BaseViewController {
+    
+    let trendingView = TrendingView()
+    let trendingViewModel = TrendingViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func loadView() {
+        view = trendingView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+        bind()
+    }
+}
+// MARK:  뷰컨 구성
+extension TrendingViewController{
+    private func configureVC() {
+        // Navigation Bar 구성
+        navigationItem.rightBarButtonItems = [trendingView.magnifyingglassItem, trendingView.sparkleItem]
     }
 }
 
-
-
+// MARK:  ViewModel Bind
+extension TrendingViewController {
+    private func bind() {
+        
+        trendingView.sparkleItem.rx.tap.bind {
+            self.tabBarController?.selectedIndex = 2
+        }.disposed(by: disposeBag)
+        
+        trendingView.magnifyingglassItem.rx.tap.bind {
+            self.tabBarController?.selectedIndex = 1
+        }.disposed(by: disposeBag)
+        
+        trendingView.playButton.rx.tap.bind {
+            print("재생버튼 탭")
+        }.disposed(by: disposeBag)
+        
+        trendingView.favoriteButton.rx.tap.bind {
+            print("찜 버튼 탭")
+        }.disposed(by: disposeBag)
+        
+    }
+}
