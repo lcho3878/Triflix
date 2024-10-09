80a515231650365d66a360dc667639fb595d0733
yeggrrr <yjkim981103@gmail.com>
2024-10-09 22:17:08 +0900

    [Rename] DetailCell 이름 변경 (DetailCell -> SimilarCell)

---
+++ Triflix/Sources/Presentation/SimilarCell.swift
@@ -0,0 +1,39 @@
+//
+//  SimilarCell.swift
+//  Triflix
+//
+//  Created by YJ on 10/9/24.
+//
+
+import UIKit
+import SnapKit
+
+final class SimilarCell: UICollectionViewCell {
+    
+    let posterImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubviews()
+        setConstraints()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    func addSubviews() {
+        contentView.addSubview(posterImageView)
+    }
+    
+    func setConstraints() {
+        posterImageView.snp.makeConstraints {
+            $0.edges.equalTo(contentView.safeAreaLayoutGuide).inset(2)
+        }
+    }
+    
+    func configureUI() {
+        posterImageView.contentMode = .scaleAspectFill
+        posterImageView.clipsToBounds = true
+    }
+}
