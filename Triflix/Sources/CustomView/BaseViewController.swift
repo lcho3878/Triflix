db9952fe5d1297e53c6cc04a10dfe7a00977316d
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-09 16:09:39 +0900

    [Chore] 기본 파일 구성

---
+++ Triflix/Sources/CustomView/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  Triflix
+//
+//  Created by 이찬호 on 10/9/24.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+}
---
+++ Triflix/Sources/Manager/Database/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Triflix
+//
+//  Created by 이찬호 on 10/9/24.
+//
+
+import Foundation
