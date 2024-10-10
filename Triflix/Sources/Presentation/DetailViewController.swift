3973843644af7a57b318b5f53c8c8fe30b896026
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-10 21:22:11 +0900

    [Fix] MediaDetail 변경사항 적용

--- Triflix/Sources/Presentation/DetailViewController.swift
+++ Triflix/Sources/Presentation/DetailViewController.swift
@@ -43,10 +43,10 @@ final class DetailViewController: UIViewController {
         // headerViewData
         output.detailData
             .bind(with: self) { owner, detailData in
-                let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(detailData.backdrop_path)")
+                let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(detailData.backdropPath)")
                 owner.detailView.posterImageView.kf.setImage(with: imageURL)
                 owner.detailView.titleLabel.text = detailData.title
-                owner.detailView.voteAverageLabel.text = String(format: "%.1f", detailData.vote_average)
+                owner.detailView.voteAverageLabel.text = String(format: "%.1f", detailData.voteAverage)
                 owner.detailView.descriptionLabel.text = detailData.overview
             }
             .disposed(by: disposeBag)
