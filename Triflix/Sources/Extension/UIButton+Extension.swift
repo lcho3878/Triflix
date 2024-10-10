0ce35b661a7c3d1331efa902ce4d601298e9d1dd
saengle <100755761+saengle@users.noreply.github.com>
2024-10-11 00:05:31 +0900

    [Fix] Logo Button

--- Triflix/Sources/Extension/UIButton+Extension.swift
+++ Triflix/Sources/Extension/UIButton+Extension.swift
@@ -10,8 +10,5 @@ import UIKit
 extension UIButton {
     func topProfileUI(imageName: String) {
             setImage(UIImage(named: imageName), for: .normal)
-            layer.borderWidth = 2
-            layer.cornerRadius = 20
-            clipsToBounds = true
         }
 }
