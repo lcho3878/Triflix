3ab6d52eb7ca39324b837a24080f99ddc20526a2
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-10 21:19:12 +0900

    [Fix] MediaDetail Codingkey적용.

--- Triflix/Sources/Manager/Network/ResponseModel/MediaDetail.swift
+++ Triflix/Sources/Manager/Network/ResponseModel/MediaDetail.swift
@@ -9,9 +9,34 @@ import Foundation
 
 struct MediaDetail: Decodable, MediaType {
     let id: Int
-    let backdrop_path: String
-    let poster_path: String
+    let backdropPath: String
+    let posterPath: String
     let title: String
     let overview: String
-    let vote_average: Double
+    let voteAverage: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case backdropPath = "backdrop_path"
+        case posterPath = "poster_path"
+        case title
+        case overview
+        case voteAverage = "vote_average"
+        case name = "name"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        backdropPath = try container.decode(String.self, forKey: .backdropPath)
+        posterPath = try container.decode(String.self, forKey: .posterPath)
+        overview = try container.decode(String.self, forKey: .overview)
+        voteAverage = try container.decode(Double.self, forKey: .voteAverage)
+        
+        if let name = try? container.decode(String.self, forKey: .name) {
+            title = name
+        } else {
+            title = try container.decode(String.self, forKey: .title)
+        }
+    }
 }
